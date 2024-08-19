fb517b4f9680ff24dfce93eec376576bcc194a45
thakkarnetram <thakkarnetram10@gmail.com>
2024-08-19 20:39:17 +0530

    initial files

---
+++ windows_processes_basic.c
@@ -0,0 +1,18 @@
+﻿// windows_processes_basic.cpp : Defines the entry point for the application.
+//
+
+#include<windows.h>
+#include<stdio.h>
+
+int main()
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&si, sizeof(si));
+	
+	CreateProcessA(NULL, "C:\\Windows\\System32\\calc.exe", NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+	return 0;
+}
